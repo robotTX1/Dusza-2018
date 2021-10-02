ae2cdbcc122521191d769dbd2c9de46b816fc142
Csaba <robottx3@gmail.com>
2021-10-02 21:00:27 +0200

    Main

--- src/com/dusza/Main.java
+++ src/com/dusza/Main.java
@@ -1,8 +1,24 @@
 package com.dusza;
 
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Path path = FileSystems.getDefault().getPath("Data", "alaprajz1.txt");
+
+        IOHandler ioHandler = new IOHandler(path);
+
+        int[][] result = ioHandler.readFile();
+
+        for(int y = 0; y<result[0].length; y++) {
+            for(int x = 0; x<result.length; x++) {
+                System.out.print(result[x][y]);
+            }
+            System.out.println();
+        }
+
+
     }
 }
